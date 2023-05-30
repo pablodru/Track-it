31586dc0de49f02fa32310529ceb0a91a1bfa766
pablodru <pdrocha03@gmail.com>
2023-05-30 17:51:52 -0300

    Inicialização de repositório

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import HabitsPage from './pages/HabitsPage';
+import TodayPage from './pages/TodayPage';
+import HistoryPage from './pages/HistoryPage';
+
+export default function App(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/cadastro' element={<RegisterPage />} />
+        <Route path='/habitos' element={<HabitsPage />} />
+        <Route path='/hoje' element={<TodayPage />} />
+        <Route path='/historico' element={<HistoryPage />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
